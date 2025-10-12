dbd5e3d2c5f23ebf86ad561382f9757bdd1f7076
RejownAhmed <rejownahmedzisan@gmail.com>
2025-10-12 11:59:53 +0600

    Fix: Overflow issue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <UContainer class="sm:border-x border-default pt-10">
+    <UContainer class="sm:border-x border-default pt-10 overflow-hidden">
       <AppHeader :links="navLinks" />
       <div class="relative">
         <div class="absolute top-0 left-0 w-full h-[600px] -z-1">
