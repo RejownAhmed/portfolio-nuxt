4a9b158bfeee81ef285c9e1783459915e0710a12
RejownAhmed <rejownahmedzisan@gmail.com>
2025-10-12 11:19:56 +0600

    ff

--- app/components/landing/WorkExperience.vue
+++ app/components/landing/WorkExperience.vue
@@ -42,7 +42,7 @@ defineProps<{
               class="inline-flex items-center gap-1"
               :style="{ color: experience.company.color }"
             >
-              <span class="font-medium">{{ experience.company.name }}</span>
+              <span class="text-sm">{{ experience.company.name }}</span>
               <UIcon :name="experience.company.logo" />
             </div>
           </ULink>
